6608a047a2bac097d7b1f5b7544df99b7794c3d4
Yahir-toronja <angel.murillo@g-tech.io>
2025-06-24 14:37:52 -0600

    cambios corregido y mas agregar formulario con zod a escuelas

--- apps/system/app/page.tsx
+++ apps/system/app/page.tsx
@@ -1,31 +1,14 @@
-import Image from "next/image";
+"use client";
+import React from "react";
+import MostrarEscuelas from "@/components/mostrar-escuelas";
 
-// import Button from "@repo/ui/src/components/shadcn/button";
-
-// import Button from "@repo/ui/src/components/shadcn/button";
 
 export default function Home() {
-  const secciones = [
-    {
-      title: "Sección 1",
-      description: "Descripción de la sección 1",
-      image: "/images/section1.png",
-    },
-    {
-      title: "Sección 2",
-      description: "Descripción de la sección 2",
-      image: "/images/section2.png",
-    },
-    {
-      title: "Sección 3",
-      description: "Descripción de la sección 3",
-      image: "/images/section3.png",
-    },
-  ];
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-       
+       <MostrarEscuelas />
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         
