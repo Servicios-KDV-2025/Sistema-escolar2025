e3911cf642f5d106b644ad5ffebf84b04a753145
JesusAldaco <jesus.ibanez@g-tech.io>
2025-07-02 16:51:48 -0600

    CRUD Alumnos

--- apps/system/app/[subdomain]/(dashboard)/escuela/[slug]/personal/page.tsx
+++ apps/system/app/[subdomain]/(dashboard)/escuela/[slug]/personal/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { TablaPersonal } from "./tabla-personal"
+import { TablaPersonal } from "../../../../../../components/tabla-personal"
 
 export default function Page() {
   return(
