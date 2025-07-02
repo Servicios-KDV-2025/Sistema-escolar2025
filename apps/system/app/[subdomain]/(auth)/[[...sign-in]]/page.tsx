7083f7fa64b4cd69b7a308b4a2d0a562d3209d23
belterrwan <ramirezibarra95@hotmail.com>
2025-07-02 15:41:24 -0600

    refactor: Update layout and enhance sign-in page
    - Adjusted the `ClerkProvider` component for better formatting.
    - Enhanced the sign-in page to display a personalized welcome message and navigation options for authenticated users.
    - Removed the unused `inicio` page to streamline the codebase.

--- apps/system/app/[subdomain]/(auth)/[[...sign-in]]/page.tsx
+++ apps/system/app/[subdomain]/(auth)/[[...sign-in]]/page.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
-import { SignIn } from '@clerk/nextjs'
+'use client'
 
-export default function Page() {
+import { Button } from '@/components/ui/button'
+import { SignIn, SignOutButton, useUser } from '@clerk/nextjs'
+import Link from 'next/link'
+import { Card, CardHeader, CardTitle, CardContent, CardFooter } from '@repo/ui/components/shadcn/card'
+
+export default function Home() {
+  const { user } = useUser()
+
+  if (!user) return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-50">
+          <SignIn />
+    </div>
+  )
+
   return (
-    <>
-      <SignIn
-      />
-    </>
+    <div className="flex items-center justify-center min-h-screen bg-gray-50">
+      <Card className="w-full max-w-md">
+        <CardHeader>
+          <CardTitle className="text-center text-2xl font-bold">Bienvenido, {user.firstName}!</CardTitle>
+        </CardHeader>
+        <CardContent className="space-y-6">
+          <div className="flex flex-col gap-3 items-center">
+            <SignOutButton />
+            <h1 className="text-xl font-semibold mt-2">Hello Page</h1>
+          </div>
+          <div className="flex flex-col gap-3 items-center">
+            <p>ir a escuela</p>
+            <Link href="/escuela">Escuela</Link>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
   )
 }
