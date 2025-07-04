ae2d3b3b7d1f0a65a6b0f9a62a6d1706190c5c5c
belterrwan <ramirezibarra95@hotmail.com>
2025-07-04 11:32:15 -0600

    refactor: Update Escuela type to make certain fields optional and add 'activa' property
    - Changed email, telefono, director, and descripcion fields to be optional.
    - Added activa property to the Escuela type for better state management.

--- apps/system/app/store/useEscuela.ts
+++ apps/system/app/store/useEscuela.ts
@@ -4,12 +4,13 @@ type Escuela = {
   _id: string;
   nombre: string;
   nombreCorto: string;
-  email: string;
-  telefono: string;
-  director: string;
-  descripcion: string;
+  email?: string;
+  telefono?: string;
+  director?: string;
+  descripcion?: string;
   direccion?: string;
   logoUrl?: string;
+  activa: boolean;
 };
 
 type EscuelaStore = {
