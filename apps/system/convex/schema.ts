9f46e4be175ce12874603a24418f2a0a6193c5af
JesusAldaco <jesus.ibanez@g-tech.io>
2025-07-11 16:21:44 -0600

    pill 11/07/2025 04:21

--- apps/system/convex/schema.ts
+++ apps/system/convex/schema.ts
@@ -152,7 +152,7 @@ const applicationTables = {
     apellidos: v.string(),
     fechaNacimiento: v.string(),
     email: v.optional(v.string()),
-    telefono: v.optional(v.string()),
+    telefono: v.optional(v.number()),
     direccion: v.optional(v.string()),
     activo: v.boolean(),
   })
